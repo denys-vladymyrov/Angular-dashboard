ad78fb2ef04ee6f788671f8af753473e80fc30bc
Denys Vladymyrov <link_amd@yahoo.com>
2025-09-12 19:56:30 +0300

    added 'default' components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Header } from './components/header/header';
+import { ServerStatus } from './components/dashboard/server-status/server-status';
+import { Traffic } from './components/dashboard/traffic/traffic';
+import { Tickets } from './components/dashboard/tickets/tickets';
+import { DashboardItem } from './components/dashboard/dashboard-item/dashboard-item';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [Header, ServerStatus, Traffic, Tickets, DashboardItem],
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('dashboard');
-}
+export class App {}
